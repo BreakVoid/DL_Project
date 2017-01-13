05b70adfbac28176b3b197c57704133285dcf7c6
BreakVoid <breakvoid@hotmail.com>
2017-01-13 20:51:40 +0800

    add featured data

--- featured_data/convert.py
+++ featured_data/convert.py
@@ -8,17 +8,40 @@ import scipy.io.wavfile as wav
 import numpy as np
 
 
-for j in xrange(40):
-    string_input = raw_input()
-    sig = string_input.split()
-    for i in xrange(len(sig)):
-        sig[i] = int(float(sig[i]))
-    sig = np.int_(sig)
-    mfcc_feat = mfcc(sig, samplerate=70)
-    print j
-    print mfcc_feat
-    print len(mfcc_feat)
-    print len(mfcc_feat[0])
-    print ("")
+import os
 
+f0_file = open("../Data/train_f0s", "r")
 
+# for j in xrange(40):
+#     string_input = raw_input()
+#     sig = string_input.split()
+#     for i in xrange(len(sig)):
+#         sig[i] = int(float(sig[i]))
+#     sig = np.int_(sig)
+#     mfcc_feat = mfcc(sig, samplerate=70)
+#     print j
+#     print mfcc_feat
+#     print len(mfcc_feat)
+#     print len(mfcc_feat[0])
+#     print ("")
+
+f0s = []
+
+for line in f0_file.readlines():
+	f0s.append(map(int, map(float, line.split())))
+
+labels = []
+for line in open("../Data/train_labels", "r").readlines():
+	labels.append(int(line))
+
+i = 0
+for f0 in f0s:
+	sig = np.int_(f0)
+	mfcc_feat = mfcc(sig, samplerate=70)
+	out_file = open("featured_data-tone-%d-%d" % (labels[i], i), "w")
+	for j in xrange(len(mfcc_feat)):
+		for k in xrange(len(mfcc_feat[j])):
+			out_file.write("%.8f " % mfcc_feat[j][k])
+		out_file.write("\n")
+	out_file.close()
+	i = i + 1
